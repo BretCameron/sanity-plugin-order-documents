b84eb29116bfa337ab444ea4189b0fbc0ed11db5
Bret Cameron <bret@yulife.com>
2020-10-11 17:49:28 +0100

    Make setListOrder capable of filtering based on start and end

--- src/functions/setOrder.js
+++ src/functions/setOrder.js
@@ -7,6 +7,13 @@ export const setOrder = (_id, index) => {
     .commit();
 };
 
-export const setListOrder = list => {
-  return Promise.all(list.map(({ _id }, index) => setOrder(_id, index)));
+export const setListOrder = (list, start = 6, end) => {
+  if (start || end) {
+    start = start || 0;
+    end = end || list.length - 1;
+
+    list = list.filter((_, index) => index >= start && index <= end);
+  }
+
+  return Promise.all(list.map(({ _id }, index) => setOrder(_id, index + (start || 0))));
 };
