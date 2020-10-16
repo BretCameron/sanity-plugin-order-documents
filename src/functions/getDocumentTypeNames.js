60ed3a313150a0fbe257c05a8dfa10f25c2b1127
Bret Cameron <bret@yulife.com>
2020-10-16 11:06:29 +0100

    Reduce iterations when getting types

--- src/functions/getDocumentTypeNames.js
+++ src/functions/getDocumentTypeNames.js
@@ -1,15 +1,18 @@
 import schema from "part:@sanity/base/schema";
+import { DEFAULT_FIELD } from "../data";
 
-export const getDocumentTypeNames = () => {
-  return schema
-    .getTypeNames()
-    .map(typeName => schema.get(typeName))
-    .filter(({ type }) => type && type.name === "document")
-    .map(type => ({ name: type.name, title: type.title }))
-    .filter(({ name }) => {
-      const fields = schema.get(name).fields
-      const hasOrderField = fields.filter((field) => field.name === 'order').length > 0
-      
-      return hasOrderField && !name.startsWith("sanity.")
-    });
+export const getDocumentTypeNames = (field = DEFAULT_FIELD) => {
+  return schema.getTypeNames().reduce((array, typeName) => {
+    const { name, title, type, fields } = schema.get(typeName);
+
+    const isDocument = type && type.name === "document";
+    const isSanity = name.startsWith("sanity.");
+    const hasOrderField = fields && fields.filter(({ name }) => name === field).length > 0;
+
+    if (isDocument && !isSanity && hasOrderField) {
+      array.push({ name, title });
+    }
+
+    return array;
+  }, []);
 };
