2c334a93eac6cf4f316fa3bb15537b7d8fd1b7a7
Bret Cameron <bret@yulife.com>
2021-04-01 17:45:06 +0100

    Update react-dnd (plus minor & patch updates)

--- src/components/molecules/Card.jsx
+++ src/components/molecules/Card.jsx
@@ -16,39 +16,55 @@ const style = {
 
 export const Card = ({ id, index, moveCard, jsx }) => {
   const ref = useRef(null);
-  const [, drop] = useDrop({
+
+  const [{ handlerId }, drop] = useDrop({
     accept: ItemTypes.CARD,
+    collect(monitor) {
+      return {
+        handlerId: monitor.getHandlerId(),
+      };
+    },
     hover(item, monitor) {
       if (!ref.current) {
         return;
       }
       const dragIndex = item.index;
       const hoverIndex = index;
+
       // Don't replace items with themselves
       if (dragIndex === hoverIndex) {
         return;
       }
+
       // Determine rectangle on screen
-      const hoverBoundingRect = ref.current && ref.current.getBoundingClientRect();
+      const hoverBoundingRect = ref.current?.getBoundingClientRect();
+
       // Get vertical middle
       const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+
       // Determine mouse position
       const clientOffset = monitor.getClientOffset();
+
       // Get pixels to the top
       const hoverClientY = clientOffset.y - hoverBoundingRect.top;
+
       // Only perform the move when the mouse has crossed half of the items height
       // When dragging downwards, only move when the cursor is below 50%
       // When dragging upwards, only move when the cursor is above 50%
+
       // Dragging downwards
       if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
         return;
       }
+
       // Dragging upwards
       if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
         return;
       }
+
       // Time to actually perform the action
       moveCard(dragIndex, hoverIndex);
+
       // Note: we're mutating the monitor item here!
       // Generally it's better to avoid mutations,
       // but it's good here for the sake of performance
@@ -56,16 +72,23 @@ export const Card = ({ id, index, moveCard, jsx }) => {
       item.index = hoverIndex;
     },
   });
+
   const [{ isDragging }, drag] = useDrag({
-    item: { type: ItemTypes.CARD, id, index },
+    type: ItemTypes.CARD,
+    item: () => {
+      return { id, index };
+    },
     collect: (monitor) => ({
       isDragging: monitor.isDragging(),
     }),
   });
+
   const opacity = isDragging ? 0 : 1;
+
   drag(drop(ref));
+
   return (
-    <div ref={ref} style={{ ...style, opacity }}>
+    <div ref={ref} style={{ ...style, opacity }} data-handler-id={handlerId}>
       <span style={{ paddingRight: "1rem" }}>{index + 1}.</span> {jsx}
     </div>
   );
